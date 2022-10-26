e379652edb27ecd7cabfa912984cf38000265cc0
teepol <78737763+teepol@users.noreply.github.com>
2022-10-26 11:39:42 +0200

    fix: refactor code, moved readme

--- app/.next/static/chunks/webpack.js
+++ app/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f4f1503db44867ac"; }
+/******/ 		__webpack_require__.h = function() { return "bb85e8fb420a7e36"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,36 +1,40 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import Logo from "./Logo";
-import {useTheme} from "next-themes";
+import { useTheme } from "next-themes";
 import { HiOutlineMoon } from "react-icons/hi2";
 
 const Header = () => {
-
-  const {systemTheme , theme, setTheme} = useTheme ();
-  const [mounted, setMounted] = useState(false);  
-  useEffect(() =>{
+  const { systemTheme, theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
     setMounted(true);
-  },[])
+  }, []);
 
-   const renderThemeChanger= () => {
-      if(!mounted) return null;
-      const currentTheme = theme === "system" ? systemTheme : theme ;
-      if(currentTheme ==="dark"){
-        return (
-          <HiOutlineMoon className="w-10 h-10 text-yellow-500 " role="button" onClick={() => setTheme('light')} />
-        )
-      }
-
-      else {
-        return (
-          <HiOutlineMoon className="w-10 h-10 text-gray-900 " role="button" onClick={() => setTheme('dark')} />
-        )
-      }
-   };
+  const renderThemeChanger = () => {
+    if (!mounted) return null;
+    const currentTheme = theme === "system" ? systemTheme : theme;
+    if (currentTheme === "dark") {
+      return (
+        <HiOutlineMoon
+          className="w-10 h-10 text-yellow-500 "
+          role="button"
+          onClick={() => setTheme("light")}
+        />
+      );
+    } else {
+      return (
+        <HiOutlineMoon
+          className="w-10 h-10 text-gray-900 "
+          role="button"
+          onClick={() => setTheme("dark")}
+        />
+      );
+    }
+  };
 
   return (
     <header className="h-15 shadow-sm dark:border-gray-700">
-      <div className="container  px-4 sm:px-6 py-4 flex justify-between items-center">       
-        
+      <div className="container  px-4 sm:px-6 py-4 flex justify-between items-center">
         {renderThemeChanger()}
       </div>
     </header>

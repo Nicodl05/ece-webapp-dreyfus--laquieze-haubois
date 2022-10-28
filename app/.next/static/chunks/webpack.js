08fa3eeae4b178f0242f0788d949036d91ea0396
Nicodl05 <78737983+Nicodl05@users.noreply.github.com>
2022-10-28 11:09:40 +0100

    feat: deleting useless function

--- app/.next/static/chunks/webpack.js
+++ app/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "08369b6066e51c12"; }
+/******/ 		__webpack_require__.h = function() { return "3d50878b67434e16"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

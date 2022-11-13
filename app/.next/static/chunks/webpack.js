a0484543aa2c4900150c87f5c1fc412dcf2f92c3
Nicodl05 <78737983+Nicodl05@users.noreply.github.com>
2022-11-13 15:44:36 +0100

    fix: begin context

--- app/.next/static/chunks/webpack.js
+++ app/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2ac0e94e989df7e3"; }
+/******/ 		__webpack_require__.h = function() { return "3d3599b095913c03"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,12 +1,15 @@
 import "../styles/global.css";
 import { ThemeProvider } from "next-themes";
 import Layout from "../components/Layout";
+import { ContextProvider } from "../components/UserContext";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider enableSystem={true} attribute="class">
       <Layout>
-        <Component {...pageProps} />
+        <ContextProvider>
+          <Component {...pageProps} />
+        </ContextProvider>
       </Layout>
     </ThemeProvider>
   );

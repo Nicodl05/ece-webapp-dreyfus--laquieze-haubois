0f83decaf448300e919285234ba70b91e3052801
Nico DL <nicolas.dreyfus@outlook.fr>
2022-11-16 16:50:01 +0100

    feat: supabase

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,14 +1,26 @@
 import "../styles/global.css";
 import { ThemeProvider } from "next-themes";
 import Layout from "../components/Layout";
+
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
 //import { ContextProvider } from "../components/UserContext";
+import { useState } from "react";
 
 function MyApp({ Component, pageProps }) {
+  const [supabase] = useState(() => createBrowserSupabaseClient());
+
   return (
     <ThemeProvider enableSystem={true} attribute="class">
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <SessionContextProvider
+        supabaseClient={supabase}
+        initialSession={pageProps.initialSession}
+      >
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SessionContextProvider>
     </ThemeProvider>
   );
 }

3dbc29b94a6cde0aee0d16b89e869d722dfa4a41
Nicodl05 <78737983+Nicodl05@users.noreply.github.com>
2022-12-13 13:00:00 +0100

    feat: add article with supa

--- app/pages/add_article.js
+++ app/pages/add_article.js
@@ -1,25 +1,138 @@
+import { useEffect, useState } from "react";
+import { supabase } from "../utils/supabase";
+var i = 0;
 function add_article() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [categorie, setCategorie] = useState("");
+  const [tags, setTags] = useState("");
+  const [id_author, setId_author] = useState("5");
+  const [formError, setFromError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!title || !description || !categorie || !tags || !id_author) {
+      setFromError("Remplissez tous les champs");
+      return;
+    }
+
+    const { data, error } = await supabase.from("articles").insert([
+      {
+        title: title,
+        description: description,
+        categorie: categorie,
+        tags: tags,
+        id: id_author,
+      },
+    ]);
+
+    if (error) {
+      console.log(error);
+      setFromError("Remplissez tous les champs correctement " + i);
+      i++;
+    }
+
+    if (data) {
+      console.log(data);
+      setFromError(null);
+    }
+  };
   return (
     <div>
-      <h1 className="text-center">Ajouter un article</h1>
-      <form>
-      <form>
-  <div class="mb-6">
-    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-    <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required>
-  </div>
-  <div class="mb-6">
-    <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-    <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
-  </div>
-  <div class="flex items-start mb-6">
-    <div class="flex items-center h-5">
-      <input id="remember" type="checkbox" value="" class="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required>
-    </div>
-    <label for="remember" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
-  </div>
-  <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-</form>
+      <h1 className=" text text-4xl text-center">Ajouter un article</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-6">
+          <label
+            htmlFor="email"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Titre de l'article
+          </label>
+          <input
+            type="text"
+            id="article"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Mon article"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            htmlFor="text"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Description
+          </label>
+          <input
+            type="text"
+            id="description"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Description de l'article"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            required
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            htmlFor="text"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Catégorie
+          </label>
+          <input
+            type="text"
+            id="categorie"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Catégorie de l'article"
+            value={categorie}
+            onChange={(e) => setCategorie(e.target.value)}
+            required
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            htmlFor="text"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Tags
+          </label>
+          <input
+            type="text"
+            id="tags"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Tags de l'article"
+            value={tags}
+            onChange={(e) => setTags(e.target.value)}
+            required
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            htmlFor="text"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Id
+          </label>
+          <input
+            type="text"
+            id="id"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Tags de l'article"
+            value={id_author}
+            onChange={(e) => setId_author(e.target.value)}
+            required
+          />
+        </div>
+        <button
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Submit
+        </button>
+        {formError && <p className="error">{formError}</p>}
       </form>
     </div>
   );
